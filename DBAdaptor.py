aac9a39de478e52117cb4eee6dc155e90a054e37
alex <jthu4alex@163.com>
2020-01-14 15:43:37 +0800

    2020/1/4 Version 0.1.1
    1\ add func: restfulAPI, include history data, node data, variable data;

--- DBAdaptor.py
+++ DBAdaptor.py
@@ -10,18 +10,36 @@
 from config import config_parser as cper
 from influxdb import InfluxDBClient
 from redis import ConnectionPool, Redis
+import pymysql
 
 
 class DBAdaptor(object):
-    def __init__(self, db_type='influxdb'):
+    def __init__(self):
+        pass
+
+    def create_conn(self, db_type='influxdb'):
         if db_type == 'redis':
-            self.conn = self.__init_redis()
+            return RedisDB()
         elif db_type == 'influxdb':
-            self.conn = self.__init_influxdb()
+            return InfluxDB()
+        elif db_type == 'mysql':
+            return MysqlDB()
         else:
             print('Error! Please specify the type of database.')
+            return None
 
-    def __init_influxdb(self):
+    def saveall(self, data):
+        pass
+
+    def createtab(self):
+        pass
+
+    def get_data(self):
+        print('factory')
+
+
+class InfluxDB(DBAdaptor):
+    def __init__(self):
         conf = cper.read_yaml_file('influxdb')
         host = conf['host']
         port = conf['port']
@@ -30,32 +48,55 @@ class DBAdaptor(object):
         database = conf['database']
         use_udp = conf['use_udp']
         udp_port = conf['udp_port']
-        client = InfluxDBClient(host=host, port=port, username=username, password=pwd, database=database)
-        if client is None:
-            print('ERROR! No DB connection was built!')
-        return client
+        self.client = InfluxDBClient(host=host, port=port, username=username, password=pwd, database=database)
+        if self.client is None:
+            print('ERROR! No InfluxDB connection was built!')
 
-    def __init_redis(self):
+    def saveall(self, data):
+        rsl = self.client.write_points(data)
+        if not rsl:
+            print('Fail to saveall!')
+
+    def exec_sql(self, sql):
+        rsl = self.client.query(sql)
+        points = rsl.get_points()
+        return points
+
+
+class RedisDB(DBAdaptor):
+    def __init__(self):
         conf = cper.read_yaml_file('redis')
         host = conf['host']
         port = conf['port']
         max_conn = conf['max_conn']
         POOL = ConnectionPool(host=host, port=port, max_connections=max_conn)
-        client = Redis(connection_pool=POOL)
-        if client is None:
-            print('ERROR! No DB connection was built!')
-        return client
-
-    def saveall(self, data):
-        rsl = self.conn.write_points(data)
-        if not rsl:
-            print('Fail to saveall!')
-
-    def createtab(self):
-        pass
+        self.client = Redis(connection_pool=POOL)
+        if self.client is None:
+            print('ERROR! No RedisDB connection was built!')
 
     def get_conn(self):
-        return self.conn
+        if self.client is not None:
+            return self.client
+        else:
+            print("Fail because there is no redis conn")
+            return None
 
 
-db = DBAdaptor()
+class MysqlDB(DBAdaptor):
+    def __init__(self):
+        conf = cper.read_yaml_file('mysql')
+        host = conf['host']
+        user = conf['user']
+        pwd = conf['password']
+        db = conf['database']
+        charset = conf['charset']
+        conn = pymysql.connect(host=host, user=user, password=pwd, database=db, charset=charset)
+        self.cursor = conn.cursor()
+
+    def get_data(self, sql):
+        rsl = self.cursor.execute(sql)
+        rsl = self.cursor.fetchall()
+        return rsl
+
+
+db_factory = DBAdaptor()
--- main.py
+++ main.py
@@ -14,10 +14,9 @@ from tornado.ioloop import IOLoop, PeriodicCallback
 from tornado.httpserver import HTTPServer
 from tornado.options import define, options
 from datetime import datetime
-from random import randint
 from tornado.escape import json_encode
 import os
-from DBAdaptor import DBAdaptor
+from DBAdaptor import db_factory
 import operator
 
 
@@ -26,7 +25,9 @@ define('port', default=8000, type=int, help='this is the port for application')
 
 clients = set()
 
-conn_redis = DBAdaptor('redis').get_conn()
+conn_redis = db_factory.create_conn('redis')
+conn_influxdb = db_factory.create_conn('influxdb')
+conn_mysql = db_factory.create_conn('mysql')
 
 data_last = None
 
@@ -71,21 +72,20 @@ def sendmsg_Ontime():
     global data_last
     gateway_id = str(1001)
     node_id = str(100002)
-    if conn_redis.hlen(gateway_id + node_id):
-        rsl = conn_redis.hgetall(gateway_id + node_id)
-        print(rsl)
+    if conn_redis.get_conn().hlen(gateway_id + node_id):
+        rsl_byte = conn_redis.get_conn().hgetall(gateway_id + node_id)
+        rsl = {k.decode(): int(v.decode()) for k, v in rsl_byte.items()}
+        print("get from redis ", rsl)
         if operator.ne(rsl, data_last):
             print('it is different than last')
-            print(data_last)
             data_last = rsl
-            print('data_last is: ', data_last)
+            dt = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
+            msg = {'dt': dt, 'data': {'tdt': dt, 'maxtc': rsl['maxtc'], 'gr': rsl['gr'], 'lc': rsl['lc'],
+                                      'lv': rsl['lv'], 'temp': rsl['temp'], 'humi': rsl['humi'], 'ev': rsl['ev']}}
+            for client in clients:
+                client.write_message(json_encode(msg))
     else:
         print('There is no data in redis.')
-    # for client in clients:
-    #     dt = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
-    #     msg = {'dt': dt, 'data': {'tdt': dt, 'maxtc': randint(0, 9), 'gr': randint(0, 15), 'lc': randint(0, 20),
-    #                               'lv': randint(0, 35), 'temp': randint(0, 30), 'humi': randint(0, 25), 'ev': randint(0, 40)}}
-    #     client.write_message(json_encode(msg))
 
 
 class ArticleHandler(RequestHandler):
@@ -105,17 +105,94 @@ class LoginHandler(RequestHandler):
         self.write('login handle')
 
 
+class HistoryDataHandler(RequestHandler):
+    def get(self):
+        dt_begin_str = self.get_argument('begin_dt', '')
+        dt_end_str = self.get_argument('end_dt', '')
+        var_id = self.get_argument('id', '1')
+        times, datas, isdetail = self._select_databydt(dt_begin_str, dt_end_str, var_id)
+        resp = {'gateway_id': '1001', 'node_id': '100002', 'time': times, 'datas': datas, 'isdetail': isdetail}
+        self.write(resp)
+
+    def _select_databydt(self, begindt_str, enddt_str, id):
+        isdetail = True
+        id_field_name = {1: 'gr', 2: 'lc', 3: 'lv', 4: 'temp', 5: 'humi', 6: 'ev', 7: 'maxtc'}
+        field_name = id_field_name[int(id)]
+        begindt = datetime.strptime(begindt_str, '%Y-%m-%d %H:%M:%S')
+        enddt = datetime.strptime(enddt_str, '%Y-%m-%d %H:%M:%S')
+        delta = enddt - begindt
+        if delta.days < 1 and delta.seconds < 7200:
+            tab_name = 'test_tab'
+            sql = "select %s from %s where time >= '%s' and time <= '%s' tz('%s')" % (field_name, tab_name, begindt_str, enddt_str, 'Asia/Shanghai')
+        else:
+            isdetail = False
+            tab_name = 'mean_min_max_1h'
+            args = (field_name + '_mean', field_name + '_max', field_name + '_min') + (tab_name, begindt_str, enddt_str, 'Asia/Shanghai')
+            sql = "select %s,%s,%s from %s where time >= '%s' and time <= '%s' tz('%s')" % args
+        print(sql)
+        rsl = conn_influxdb.exec_sql(sql)
+        times = []
+        datas = []
+        if rsl:
+            if isdetail:
+                for r in rsl:
+                    times.append(r['time'])
+                    datas.append(r[field_name])
+            else:
+                for r in rsl:
+                    times.append(r['time'])
+                    data = {'mean': r[field_name + '_mean'], 'max': r[field_name + '_max'], 'min': r[field_name + '_min']}
+                    print(data)
+                    datas.append(data)
+        return times, datas, isdetail
+
+
+
+class NodeDataHandler(RequestHandler):
+    def get(self):
+        gateway_id = '1001'
+        sql = 'select * from info_nodes where gateway_id=%s order by node_id' % gateway_id
+        print(sql)
+        rsl = conn_mysql.get_data(sql)
+        print(rsl)
+        if rsl:
+            resp = {'datas': [{'id': r[0], 'node_id': r[1]} for r in rsl]}
+        else:
+            resp = {'datas': []}
+        self.write(resp)
+
+
+class VariableDataHandler(RequestHandler):
+    def get(self):
+        node_id = self.get_argument('node_id', '1')
+        sql = 'select info_maptab_var_tabname.id,info_variable.name,info_maptab_var_tabname.tab_name from info_variable inner join info_maptab_var_tabname on(info_variable.id=info_maptab_var_tabname.variable_id) where node_id=%d' % int(node_id)
+        rsl = conn_mysql.get_data(sql)
+        print(rsl)
+        if rsl:
+            resp = {'datas': [{'id': r[0], 'var_name': r[1]} for r in rsl]}
+        else:
+            resp = {'datas': []}
+        self.write(resp)
+
+
 if __name__ == '__main__':
+    settings = {
+        'template_path': os.path.join(os.path.dirname(__file__), "templates"),
+        'static_path': os.path.join(os.path.dirname(__file__), "static"),
+        'debug': True,
+    }
+
     app = Application(
         [
             (r'/', IndexHandler),
             (r'/article', ArticleHandler, {'title': 'test title'}),
             url(r'/login', LoginHandler, name='login'),
-            (r'/ws', WSHandler)
+            (r'/ws', WSHandler),
+            (r'/api/datas/history', HistoryDataHandler),
+            (r'/api/datas/nodes', NodeDataHandler),
+            (r'/api/datas/variables', VariableDataHandler),
         ],
-        template_path=os.path.join(os.path.dirname(__file__), "templates"),
-        static_path=os.path.join(os.path.dirname(__file__), "static"),
-        debug=True)
+        **settings)
     http_server = HTTPServer(app)
     options.parse_command_line()
 
